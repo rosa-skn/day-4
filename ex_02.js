bf468997df998250a4fe466d15e12fa73ef41538
rosa-skn <rosa.sokun@epitech.digital>
2024-10-03 17:23:51 +0200

    Update ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -1,1 +1,5 @@
-document.getElementById('description').innerHTML="0<br/>1<br/>2"
+document.addEventListener("DOMContentLoaded", function() {
+    let text = document.getElementsByTagName('p');
+    for (var i = 0; i < text.length; i++) {
+        text[i].innerHTML = i; 
+    }});
